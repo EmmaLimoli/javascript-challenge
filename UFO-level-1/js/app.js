cd331e3d151a71b4c447ef2e3f332d6cbf7879f5
Emma Limoli <EmmaLimoli024@gmail.com>
2020-09-13 17:33:32 -0400

    worked on table

--- UFO-level-1/js/app.js
+++ UFO-level-1/js/app.js
@@ -1,5 +1,8 @@
 // from data.js
+// this is the source
 var tableData = data;
+// create the destination
+var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
 // console.log data from data.js
@@ -10,16 +13,27 @@ var tableData = data;
 //     console.log(alienSightings);
 // });
 
+// start transfering the data, iterate through the source
 // use d3 to push one table row to alienSightings object and object entries to see key and values
 data.forEach(function(alienSightings) {
     console.log(alienSightings);
-    var row = data.push("tr");
+    var row = tbody.append("tr");
     Object.entries(alienSightings).forEach(function([key, value]) {
     console.log(key, value);
-    // var cell = row.push("td");
+    var cell = row.append("td");
+    cell.text(value);
     });  
 });   
 
+// var btnRef = document.getElementById("filter-btn");
+// btnRef.addEventListener("click", function(){
+//     alert("Finally!!!")
+// });
+// console.log(btnRef);
 
 
+
+
+var body = d3.select("body");
+console.log(body)
 
